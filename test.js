76a3d07ce9a711bc5905ab9649fda0162837336f
Grayson Earle <graysonearle@gmail.com>
2024-04-29 21:22:34 +0200

    updated test

--- test.js
+++ test.js
@@ -1,26 +1,14 @@
-const STLSDR = require('stl-sdr');
+const rtljs = require("rtljs");
 
-const sdr = new STLSDR();
+console.log(rtljs.getDeviceCount()); // 1
+console.log(rtljs.getDeviceName(0)); // Generic RTL R820T2
 
-const frequency = 96.3e6; // tune to 96.3 MHz
-const sampleRate = 2.4e6; // sample rate of 2.4 Msps
-const gain = 40; // tuner gain of 40 dB
+let device = rtljs.open(0);
+device.setCenterFreq(1090 * rtljs.mhz); // 1090000000
 
-sdr.open();
-sdr.setFrequencyCorrection(0);
-sdr.setSampleRate(sampleRate);
-sdr.setFrequency(frequency);
-sdr.setTunerGain(gain);
+// raw IQ data
+device.resetBuffer(); // reset buffer to prevent communication data from appearing as radio data
+let data = device.readSync(512); // read 512b
+console.log(JSON.stringify(data)); // [128, 127, 128... etc
 
-const buffer = new Buffer(sampleRate * 2);
-
-// receive samples
-sdr.readSync(buffer, buffer.length);
-
-// process samples
-for (let i = 0; i < buffer.length; i += 2) {
-    const sample = buffer.readInt16LE(i);
-    console.log(sample);
-}
-
-sdr.close();
+rtljs.close(device);
