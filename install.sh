2d244ee9cf4b49acc279abd33c8ee77c06d4f405
Grayson Earle <graysonearle@gmail.com>
2024-06-25 00:09:17 +0200

    updated install script

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Update PATH to include Node.js binaries
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+export PATH="$HOME/.nvm/versions/node/v22.3.0/bin:$PATH"
+
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
