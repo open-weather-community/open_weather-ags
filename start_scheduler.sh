962db5948537f19f5d4c9ccf66b2fee2bed71f18
Grayson Earle <graysonearle@gmail.com>
2024-08-06 10:38:31 +0200

    updated startup script to do git reset

--- start_scheduler.sh
+++ start_scheduler.sh
@@ -8,9 +8,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
-# Log environment variables for debugging
-#env >> /home/openweather/cronlog.txt
-
 # Introduce a delay to allow the drive to mount
 sleep 30
 
@@ -18,11 +15,11 @@ sleep 30
 #echo "Changing directory to /home/openweather/open_weather-ags" >> /home/openweather/cronlog.txt
 cd /home/openweather/open_weather-ags 2>> /home/openweather/cronlog.txt
 
-# Log the current working directory for debugging
-#echo "Current working directory after cd: $(pwd)" >> /home/openweather/cronlog.txt
+# Fetch latest changes from Git repository
+git fetch origin >> /home/openweather/cronlog.txt 2>&1
 
-# Pull latest changes from Git repository (assuming origin/main is the branch)
-git pull origin main >> /home/openweather/cronlog.txt 2>&1
+# Reset the local branch to match the remote branch
+git reset --hard origin/main >> /home/openweather/cronlog.txt 2>&1
 
 # Launch the Node.js process using the node executable in the current working directory
 /home/openweather/.nvm/versions/node/v22.3.0/bin/node scheduler.js >> /home/openweather/cronlog.txt 2>&1
